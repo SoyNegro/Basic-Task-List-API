80b0f6388c996240163d495a366b6bde3319c270
SoyNegro <krisoul.valdes@gmail.com>
2022-05-02 11:10:36 -0400

    TaskListController test cases husk

--- src/test/java/com/thedarksideofcode/TaskList/controller/TaskListControllerTest.java
+++ src/test/java/com/thedarksideofcode/TaskList/controller/TaskListControllerTest.java
@@ -1,4 +1,47 @@
 package com.thedarksideofcode.TaskList.controller;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(controllers = TaskListController.class)
+@ExtendWith(MockitoExtension.class)
 public class TaskListControllerTest {
+
+
+    @Test
+    void getAllTasks(){
+
+    }
+
+    @Test
+    void givenIdShouldReturnTaskOfId(){
+
+    }
+
+    @Test
+    void givenWrongIdShouldReturnNotFound(){
+
+    }
+
+    @Test
+    void givenTaskToSaveShouldReturnSavedTask(){
+
+    }
+
+    @Test
+    void givenIdAndTaskToUpdateShouldReturnUpdatedTask(){
+
+    }
+
+    @Test
+    void givenIdAndEmptyTaskToUpdateShouldReturnInvalidTask(){
+
+    }
+
+    @Test
+    void givenIdToDeleteShouldDeleteTaskOfId(){
+
+    }
 }
