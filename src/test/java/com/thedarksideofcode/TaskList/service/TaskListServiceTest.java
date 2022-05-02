db314f4c57c2bacb07a49102ba082bf3b9aef327
SoyNegro <krisoul.valdes@gmail.com>
2022-05-02 16:11:25 -0400

    TaskListServiceTest test husk created

--- src/test/java/com/thedarksideofcode/TaskList/service/TaskListServiceTest.java
+++ src/test/java/com/thedarksideofcode/TaskList/service/TaskListServiceTest.java
@@ -1,4 +1,39 @@
 package com.thedarksideofcode.TaskList.service;
 
+import com.thedarksideofcode.TaskList.model.BasicTask;
+import com.thedarksideofcode.TaskList.repository.BasicTaskRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@ExtendWith(MockitoExtension.class)
 public class TaskListServiceTest {
+
+    @Mock
+    BasicTaskRepository basicTaskRepository;
+
+    @InjectMocks
+    TaskListService taskListService;
+
+    private BasicTask basicTask1;
+
+    private ResponseEntity<BasicTask> okResponseEntity;
+    private ResponseEntity<BasicTask> createdResponseEntity;
+    private ResponseEntity<String> notFoundResponseEntity;
+    private ResponseEntity<String> badRequestResponseEntity;
+
+    @BeforeEach
+    void settingUp(){
+     basicTask1 = new BasicTask("EasyFakeId1","Will test the different responses");
+     okResponseEntity = new ResponseEntity<>(basicTask1, HttpStatus.OK);
+     createdResponseEntity =  new ResponseEntity<>(basicTask1, HttpStatus.CREATED);
+     notFoundResponseEntity =  new ResponseEntity<>(HttpStatus.NOT_FOUND);
+     badRequestResponseEntity =  new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
 }
